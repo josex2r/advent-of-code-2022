b37ee7f7123d4287e345cde3c0b4da7fe6a48be5
josex2r <jlrepresa@gmail.com>
2022-12-03 07:28:57 +0100

    feat: day 3

---
+++ day-3/part-2.ts
@@ -0,0 +1,34 @@
+import { buffer, path } from "../deps.ts";
+
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const filePath = path.join(__dirname, "input.txt");
+const inputFile = await Deno.open(filePath);
+
+const lines = buffer.readLines(inputFile);
+let iterator = await lines.next();
+
+const getLowerValue = (str: string) =>
+  str.charCodeAt(0) - "a".charCodeAt(0) + 1;
+const getUpperValue = (str: string) =>
+  str.charCodeAt(0) - "A".charCodeAt(0) + 27;
+const isUpper = (str: string) => str.charCodeAt(0) <= "Z".charCodeAt(0);
+const getValue = (str: string): number =>
+  isUpper(str) ? getUpperValue(str) : getLowerValue(str);
+
+let result = 0;
+
+while (!iterator.done) {
+  const line1 = iterator.value;
+  const line2 = (await lines.next()).value;
+  const line3 = (await lines.next()).value;
+
+  const common = line1
+    .split("")
+    .find((char) => line2.includes(char) && line3.includes(char)) as string;
+
+  result += getValue(common);
+
+  iterator = await lines.next();
+}
+
+console.log(`Sum of common chars: ${result}`);
